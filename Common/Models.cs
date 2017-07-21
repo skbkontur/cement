9ccec201a02f5de1262ca9bbc488e889b0d71f3f
kungurtsev <kungurtsev@skbkontur.ru>
2017-07-21 13:06:54 +0500

    delete extra info

--- Common/Models.cs
+++ Common/Models.cs
@@ -2,17 +2,11 @@
 {
     public class InfoResponseModel
     {
-        public string Package { get; }
-        public string Branch { get; }
         public string CommitHash { get; }
-        public string Created { get; }
-
-        public InfoResponseModel(string package, string branch, string commitHash, string created)
+        
+        public InfoResponseModel(string commitHash)
         {
-            Package = package;
-            Branch = branch;
             CommitHash = commitHash;
-            Created = created;
         }
     }
 }
