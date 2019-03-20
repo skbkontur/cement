7f40d7b7954cabd9126ee6ee7e0c9fc3db40596c
Evgeniy Degtyarev <edegtyarev@skbkontur.ru>
2019-03-20 14:25:34 +0500

    do not kill msbuild on every module build

--- Common/ModuleBuilderHelper.cs
+++ Common/ModuleBuilderHelper.cs
@@ -45,7 +45,7 @@ namespace Common
 
         private static List<KeyValuePair<string, string>> msBuildsCache;
 
-        private static List<KeyValuePair<string, string>> FindAvilableMsBuilds()
+        public static List<KeyValuePair<string, string>> FindAvilableMsBuilds()
         {
             if (msBuildsCache != null)
                 return msBuildsCache;
