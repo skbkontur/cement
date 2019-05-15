f8b42b94001df90bb49f2faabd3e2fa98b1a5371
Alexey Smirnov <alexey.smirnov@skbkontur.ru>
2019-05-15 20:53:31 +0500

    Adds ConfigurationHierarchy and tests
    ConfigurationHierarchy takes parsed config lines and makes it possible to return parent configurations.
    It's necessary for some sections: they require inheritance support (i.e. deps section)

---
+++ Common/YamlParsers/ConfigurationHierarchy.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.YamlParsers.Models;
+using JetBrains.Annotations;
+
+namespace Common.YamlParsers
+{
+    public class ConfigurationHierarchy
+    {
+        private const string defaultConfigName = "full-build";
+        private readonly ConfigurationLine[] configs;
+        private readonly ConfigurationLine defaultConfig;
+        private readonly IReadOnlyDictionary<string, string[]> configNameToParentsMap;
+        private string[] getAllCached;
+
+        public ConfigurationHierarchy(ConfigurationLine[] configs)
+        {
+            this.configs = configs;
+            defaultConfig = DetermineDefaultConfig(configs);
+            configNameToParentsMap = configs.ToDictionary(config => config.ConfigName, config => config.ParentConfigs);
+        }
+
+        /// <summary>
+        /// Return all ancestors of a given configuration
+        /// </summary>
+        [CanBeNull]
+        public string[] FindParents(string configName)
+        {
+            if (configNameToParentsMap[configName] == null)
+                return null;
+
+            var queue = new Queue<string>();
+            queue.Enqueue(configName);
+
+            var parents = new List<string>();
+
+            while (queue.Any())
+            {
+                var current = queue.Dequeue();
+                if (configNameToParentsMap[current] == null)
+                    continue;
+
+                foreach (var parent in configNameToParentsMap[current])
+                {
+                    queue.Enqueue(parent);
+                    parents.Add(parent);
+                }
+            }
+            return parents.ToArray();
+        }
+
+        /// <summary>
+        /// Return default configuration name
+        /// </summary>
+        [NotNull]
+        public string GetDefault()
+        {
+            return defaultConfig.ConfigName;
+        }
+
+        /// <summary>
+        /// Return all configurations in order:
+        /// From smallest config with no deps to complex configs with multiple ancestors.
+        /// </summary>
+        [NotNull]
+        public string[] GetAll()
+        {
+            return getAllCached ?? (getAllCached = GetAllInner().ToArray());
+        }
+
+        [NotNull]
+        private IEnumerable<string> GetAllInner()
+        {
+            var roots = configNameToParentsMap
+                .Where(kvp => kvp.Value == null)
+                .Select(kvp => kvp.Key)
+                .ToArray();
+
+            var queue = new Queue<string>();
+            foreach(var root in roots)
+                queue.Enqueue(root);
+
+            while (queue.Any())
+            {
+                var node = queue.Dequeue();
+                var children = configNameToParentsMap
+                    .Where(kvp => kvp.Value != null && kvp.Value.Contains(node))
+                    .Select(kvp => kvp.Key);
+
+                foreach (var child in children)
+                {
+                    if (!queue.Contains(child))
+                        queue.Enqueue(child);
+                }
+
+                yield return node;
+            }
+        }
+
+        [NotNull]
+        private static ConfigurationLine DetermineDefaultConfig(ConfigurationLine[] lines)
+        {
+            var config = lines.FirstOrDefault(l => l.IsDefault) ??
+                         lines.FirstOrDefault(l => string.Equals(l.ConfigName, defaultConfigName));
+
+            if (config != null)
+                return config;
+
+            if (lines.Length == 1)
+                return lines[0];
+
+            throw new ArgumentException("Cannot determine default module configuration. Specify it via '*default' keyword.", nameof(configs));
+        }
+    }
+}
---
+++ Tests/ParsersTests/Yaml/TestConfigurationHierarchy.cs
@@ -0,0 +1,233 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.YamlParsers;
+using NUnit.Framework;
+
+namespace Tests.ParsersTests.Yaml
+{
+    [TestFixture]
+    public class TestConfigurationHierarchy
+    {
+        private readonly ConfigLineParser parser;
+
+        public TestConfigurationHierarchy()
+        {
+            this.parser = new ConfigLineParser();
+        }
+
+        [TestCaseSource(nameof(FindParentSource))]
+        public void FindParents(string childConfig, string[] configLines, string[] expectedParents)
+        {
+            var hierarchy = GetHierarchy(configLines);
+            var actualParents = hierarchy.FindParents(childConfig);
+            Assert.AreEqual(expectedParents, actualParents);
+        }
+
+        [TestCaseSource(nameof(FindDefaultSource))]
+        public string FindDefault(List<string> configLines)
+        {
+            var hierarchy = GetHierarchy(configLines);
+            return hierarchy.GetDefault();
+        }
+
+        [TestCaseSource(nameof(GetAllSource))]
+        public void GetAll(string[] configLines, string[] expectedAll)
+        {
+            var hierarchy = GetHierarchy(configLines);
+            var actualAll = hierarchy.GetAll();
+            var msg = "Actual: " + string.Join(", ", actualAll)
+                                 + Environment.NewLine
+                                 + "Expected: " + string.Join(", ", expectedAll);
+
+            Assert.AreEqual(expectedAll, actualAll, msg);
+        }
+
+        private TestCaseData[] FindParentSource =
+        {
+            new TestCaseData("A", new[] { "A" }, null)
+                .SetName("Single A without parent"),
+            new TestCaseData("B", new[]
+            {
+                "A *default",
+                "B > A",
+            }, new[] { "A" })
+                .SetName("One root, one child"),
+
+            new TestCaseData("C", new[]
+            {
+                "A *default",
+                "B > A",
+                "C > A"
+            }, new[] { "A" })
+                .SetName("One root, 2 children"),
+
+            new TestCaseData("D", new[]
+            {
+                "A",
+                "B > A",
+                "C > A",
+                "D > A *default",
+            }, new[] { "A" })
+                .SetName("One root, 2 usual children and one marked as default"),
+
+            new TestCaseData("E", new[]
+            {
+                "A",
+                "B > A",
+                "C > A",
+                "D > A *default",
+                "E > B"
+            }, new[] { "B", "A" })
+                .SetName("Two roots"),
+
+/*
+                A    F
+               /|\   |
+              B C D' G
+              \     /
+               E   /
+                \ /
+                 H
+*/
+            new TestCaseData("H", new[]
+            {
+                "A",
+                "B > A",
+                "C > A",
+                "D > A *default",
+                "E > B",
+                "F",
+                "G > F",
+                "H > E, G"
+
+            }, new[] { "E", "G", "B", "F", "A" })
+                .SetName("Complex tree"),
+        };
+
+        private TestCaseData[] FindDefaultSource =
+        {
+            new TestCaseData(new List<string> { "A" })
+                .Returns("A")
+                .SetName("Single config is default"),
+
+            new TestCaseData(new List<string> { "A *default" })
+                .Returns("A")
+                .SetName("Single config marked as default"),
+
+            new TestCaseData(new List<string>
+            {
+                "A",
+                "B *default",
+            })
+                .Returns("B")
+                .SetName("Two non-full-build configs, second of them marked as default"),
+
+            new TestCaseData(new List<string>
+            {
+                "A *default",
+                "B",
+            })
+                .Returns("A")
+                .SetName("Two non-full-build configs, first of them marked as default"),
+
+            new TestCaseData(new List<string>
+            {
+                "A",
+                "B",
+                "full-build"
+            })
+                .Returns("full-build")
+                .SetName("Two non-full-build configs and a full-build, no default mark"),
+
+            new TestCaseData(new List<string>
+            {
+                "A *default",
+                "B",
+                "full-build"
+            })
+                .Returns("A")
+                .SetName("Two non-full-build configs and a full-build, first marked as default"),
+
+            new TestCaseData(new List<string>
+            {
+                "A > B *default",
+                "B",
+                "full-build"
+            })
+                .Returns("A")
+                .SetName("Two inhereted non-full-build configs and a full-build, first marked as default"),
+
+            new TestCaseData(new List<string> { "A", "B" })
+                .Throws(typeof(ArgumentException))
+                .SetName("Throw ArgumentException on several non-full-build configs without default mark"),
+        };
+
+        private TestCaseData[] GetAllSource =
+        {
+            new TestCaseData(new[] { "A" }, new[] { "A" })
+                .SetName("A"),
+
+            new TestCaseData(new[]
+                {
+                    "A",
+                    "B *default"
+                }, new[] { "A", "B" })
+                .SetName("A; B"),
+
+            new TestCaseData(new[]
+                {
+                    "A",
+                    "B *default",
+                    "C > A"
+
+                }, new[] { "A", "B", "C" })
+                .SetName("A; B; C > A"),
+
+
+/*
+              A   D
+             / \ /
+            B'  C
+ */
+            new TestCaseData(new[]
+                {
+                    "A",
+                    "B > A *default",
+                    "C > A, D",
+                    "D",
+
+                }, new[] { "A", "D", "B", "C" })
+                .SetName("A; D; B>A; C>A,D"),
+
+            /*
+                A    F
+               /|\   |
+              B C D' G
+              \     /
+               E   /
+                \ /
+                 H
+*/
+            new TestCaseData(new[]
+                {
+                    "A",
+                    "B > A",
+                    "C > A",
+                    "D > A *default",
+                    "E > B",
+                    "F",
+                    "G > F",
+                    "H > E, G"
+
+                }, new[] { "A", "F", "B", "C", "D", "G", "E", "H" })
+                .SetName("Complex tree"),
+        };
+
+        private ConfigurationHierarchy GetHierarchy(IEnumerable<string> configLines)
+        {
+            var lines = configLines.Select(line => parser.Parse(line)).ToArray();
+            return new ConfigurationHierarchy(lines);
+        }
+    }
+}
