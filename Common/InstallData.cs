868613e099ee6472b40d455f2a254101a2c5ccf7
kungurtsev <kungurtsev@skbkontur.ru>
2019-05-21 16:08:56 +0500

    clean xml docs

--- Common/InstallData.cs
+++ Common/InstallData.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace Common
 {
@@ -7,33 +7,33 @@ namespace Common
         public string ModuleName { get; set; }
 
         /// <summary>
-        /// Installs of current configuration
-        /// 1. contains results of build. Those who want to reference this module/configuration should reference these results.
-        /// 2. contains artifacts (files that can be used by other modules, but are ignored while running `cm ref add`)
-        /// 3. does not contain nuget and external modules
+        /// <para>Installs of current configuration.</para>
+        /// <para>1. Contains results of build. Those who want to reference this module/configuration should reference these results.</para>
+        /// <para>2. Contains artifacts (files that can be used by other modules, but are ignored while running `cm ref add`).</para>
+        /// <para>3. Does not contain nuget and external modules.</para>
         /// </summary>
         public List<string> MainConfigBuildFiles { get; set; }
 
         /// <summary>
-        /// Install files of current and parent configuration
-        /// 1. contains results of build. Those who want to reference this module/configuration should reference these results.
-        /// 2. does not contain artifacts
-        /// 3. does not contain nuget and external modules
+        /// <para>Install files of current and parent configuration.</para>
+        /// <para>1. Contains results of build. Those who want to reference this module/configuration may reference these results.</para>
+        /// <para>2. Does not contain artifacts.</para>
+        /// <para>3. Does not contain nuget and external modules.</para>
         /// </summary>
         public List<string> BuildFiles { get; set; }
 
         /// <summary>
-        /// Install modules of current and parent configurations
+        /// Install modules of current and parent configurations.
         /// </summary>
         public List<string> ExternalModules { get; set; }
 
         /// <summary>
-        /// Install nuget of current and parent configurations
+        /// Install nuget of current and parent configurations.
         /// </summary>
         public List<string> NuGetPackages { get; set; }
 
         /// <summary>
-        /// Install and artifacts of current and parent configurations
+        /// Install and artifacts of current and parent configurations.
         /// </summary>
         public List<string> Artifacts { get; set; }
 
