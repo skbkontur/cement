ed558f8ae87efb7d9d00466a971e7f50f30e4cf8
Alexey Smirnov <alexey.smirnov@skbkontur.ru>
2019-05-22 16:37:41 +0500

    InstallData.JoinWith now does Distinct(), added tests for InstallData.JoinWith extension method

--- Common/Extensions/InstallDataExtensions.cs
+++ Common/Extensions/InstallDataExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
@@ -13,14 +14,21 @@ namespace Common.Extensions
         {
             var result = new InstallData
             {
-                Artifacts = new List<string>(first.Artifacts.Concat(second.Artifacts)),
-                ExternalModules = new List<string>(first.ExternalModules.Concat(second.ExternalModules)),
-                InstallFiles = new List<string>(first.InstallFiles.Concat(second.InstallFiles)),
-                NuGetPackages = new List<string>(first.NuGetPackages.Concat(second.NuGetPackages)),
-                CurrentConfigurationInstallFiles = new List<string>(first.CurrentConfigurationInstallFiles.Concat(second.CurrentConfigurationInstallFiles)),
+                Artifacts = new List<string>(ConcatDistinct(first, second, item => item.Artifacts)),
+                ExternalModules = new List<string>(ConcatDistinct(first, second, item => item.ExternalModules)),
+                InstallFiles = new List<string>(ConcatDistinct(first, second, item => item.InstallFiles)),
+                NuGetPackages = new List<string>(ConcatDistinct(first, second, item => item.NuGetPackages)),
+                CurrentConfigurationInstallFiles = new List<string>(ConcatDistinct(first, second, item => item.CurrentConfigurationInstallFiles)),
             };
 
             return result;
+        }
+
+        private static IEnumerable<string> ConcatDistinct(InstallData a, InstallData b, Func<InstallData, IEnumerable<string>> getCollectionFunc)
+        {
+            var firstCollection = getCollectionFunc(a);
+            var secondCollection = getCollectionFunc(b);
+            return firstCollection.Concat(secondCollection).Distinct();
         }
     }
 }
