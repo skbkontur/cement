63d70a890a8a69703c066965196021afb7a793c1
kungurtsev <kungurtsev@skbkontur.ru>
2019-10-23 13:59:49 +0500

    fix dotnet check-deps

--- Common/DepsChecker.cs
+++ Common/DepsChecker.cs
@@ -30,7 +30,7 @@ namespace Common
             var refsList = new List<ReferenceWithCsproj>();
             foreach (var bulid in buildData)
             {
-                if (bulid.Target.IsFakeTarget() || bulid.Tool.Name != "msbuild")
+                if (bulid.Target.IsFakeTarget() || (bulid.Tool.Name != "msbuild" && bulid.Tool.Name != "dotnet"))
                     continue;
                 var vsParser = new VisualStudioProjectParser(Path.Combine(moduleDirectory, bulid.Target), modules);
                 var files = vsParser.GetCsprojList(bulid);
