4418b0baeeff87206145a2bb3369261b3957db9b
Isakov Anatolii <a.isakov@skbkontur.ru>
2022-06-20 17:07:43 +0500

    fix: починены install-скрипты для linux и osx

--- files-common/osx-x64/install.sh
+++ files-common/osx-x64/install.sh
@@ -2,7 +2,16 @@
 
 rm -rf ~/.cement
 rm -rf ~/bin/dotnet
+
 mkdir ~/bin
+
+cp -R ../../dotnet ~/.cement
 cp -R ../../dotnet ~/bin/dotnet
-osx-x64/cm.exe reinstall
+
+cp -R ~/bin/dotnet/linux-x64/cm ~/bin/dotnet/cm.exe
+
+rm -rf ~/bin/dotnet/linux-x64/cm
+
+./cm reinstall
+
 chmod +x ~/bin/cm
