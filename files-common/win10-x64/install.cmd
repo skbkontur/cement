09d27d23b5acd578ceab0275e67b8af37a5d94f4
dv.kab <dv.kab@skbkontur.ru>
2022-06-20 12:13:42 +0300

    fix(install): set cm reinstall run folder to root

--- files-common/win10-x64/install.cmd
+++ files-common/win10-x64/install.cmd
@@ -2,5 +2,5 @@
 rmdir /S /Q %userprofile%\.cement
 rmdir /S /Q %userprofile%\bin\dotnet
 xcopy ..\..\dotnet "%userprofile%\bin\dotnet" /s /i /Y
-win10-x64\cm.exe reinstall
+.\cm.exe reinstall
 %userprofile%\bin\cm.cmd
