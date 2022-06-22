381e3de6a6dd6a20ed7d01897985abde836b4468
Кабанов Данил Владимирович <dv.kab@skbkontur.ru>
2022-06-22 14:11:56 +0500

    ci: copy NuGet.exe on publish

--- files-common/win10-x64/install.cmd
+++ files-common/win10-x64/install.cmd
@@ -1,6 +1,7 @@
 @echo off
 rmdir /S /Q %userprofile%\.cement
 rmdir /S /Q %userprofile%\bin\dotnet
+xcopy /Y .\NuGet.exe ..\..\dotnet\NuGet.exe
 xcopy ..\..\dotnet "%userprofile%\bin\dotnet" /s /i /Y
 .\cm.exe reinstall
 %userprofile%\bin\cm.cmd
