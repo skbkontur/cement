3c749208522cff7c76b2b4653a3c7417131e82c5
dv.kab <dv.kab@skbkontur.ru>
2022-06-30 10:29:49 +0300

    fix(install): copy cm binary in win install scipt

--- files-common/win10-x64/install.cmd
+++ files-common/win10-x64/install.cmd
@@ -1,7 +1,12 @@
 @echo off
 rmdir /S /Q %userprofile%\.cement
 rmdir /S /Q %userprofile%\bin\dotnet
+
 copy /Y .\NuGet.exe ..\..\dotnet\NuGet.exe
+
 xcopy ..\..\dotnet "%userprofile%\bin\dotnet" /s /i /Y
+
+copy /Y %userprofile%\bin\dotnet\win10-x64\cm.exe %userprofile%\bin\dotnet\cm.exe
+
 .\cm.exe reinstall
 %userprofile%\bin\cm.cmd
