55711df9ba147705ab690cc74c50dfcb7f2386ac
Denis Tarasov <dstarasov@skbkontur.ru>
2022-09-21 20:23:26 +0500

    refactor(common): Script и BuildData теперь auto-свойства

--- Common/BuildScriptWithBuildData.cs
+++ Common/BuildScriptWithBuildData.cs
@@ -2,13 +2,14 @@
 {
     public sealed class BuildScriptWithBuildData
     {
-        public readonly string Script;
-        public readonly BuildData BuildData;
-
         public BuildScriptWithBuildData(string script, BuildData buildData)
         {
             Script = script;
             BuildData = buildData;
         }
+
+        public string Script { get; }
+
+        public BuildData BuildData { get; }
     }
 }
