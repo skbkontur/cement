415f47568b74f72e61b3091a036319206d953c6a
Denis Tarasov <dstarasov@skbkontur.ru>
2022-09-21 20:25:26 +0500

    refactor(common): все поля классаBuildSettings теперь auto-свойства

--- Common/BuildSettings.cs
+++ Common/BuildSettings.cs
@@ -2,11 +2,16 @@
 {
     public sealed class BuildSettings
     {
-        public bool ShowObsoleteWarnings;
-        public bool ShowAllWarnings;
-        public bool ShowOutput;
-        public bool ShowProgress;
-        public bool ShowWarningsSummary;
-        public bool CleanBeforeBuild;
+        public bool ShowObsoleteWarnings { get; set; }
+
+        public bool ShowAllWarnings { get; set; }
+
+        public bool ShowOutput { get; set; }
+
+        public bool ShowProgress { get; set; }
+
+        public bool ShowWarningsSummary { get; set; }
+
+        public bool CleanBeforeBuild { get; set; }
     }
 }
