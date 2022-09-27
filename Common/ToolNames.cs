23c710f2a7d0eec9c7e7a63ab156672d587c9761
Denis Tarasov <dstarasov@skbkontur.ru>
2022-09-27 09:23:54 +0500

    style: заигнорил предупреждение об Inconsistent Naming

--- Common/ToolNames.cs
+++ Common/ToolNames.cs
@@ -1,9 +1,8 @@
 namespace Common;
 
+// ReSharper disable InconsistentNaming
 public static class ToolNames
 {
-    // ReSharper disable once InconsistentNaming
     public const string MSBuild = "msbuild";
-
     public const string DOTNET = "dotnet";
 }
