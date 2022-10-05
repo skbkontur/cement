0e2eb15e08db7d005a347d3a2e4cc7188aeaaf57
Denis Tarasov <dstarasov@skbkontur.ru>
2022-10-05 21:33:24 +0500

    refactor: вынес SelfUpdate в отдельный файл

---
+++ Commands/SelfUpdate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Common;
+using Common.Logging;
+using Microsoft.Extensions.Logging;
+
+namespace Commands;
+
+public static class SelfUpdate
+{
+    public static void UpdateIfOld(FeatureFlags featureFlags)
+    {
+        var consoleWriter = ConsoleWriter.Shared;
+        var log = LogManager.GetLogger(nameof(SelfUpdate));
+
+        try
+        {
+            var isEnabledSelfUpdate = CementSettingsRepository.Get().IsEnabledSelfUpdate;
+            if (isEnabledSelfUpdate.HasValue && !isEnabledSelfUpdate.Value)
+                return;
+            var lastUpdate = Helper.GetLastUpdateTime();
+            var now = DateTime.Now;
+            var diff = now - lastUpdate;
+            if (diff <= TimeSpan.FromHours(5))
+                return;
+
+            var selfUpdateCommand = new SelfUpdateCommand(consoleWriter, featureFlags)
+            {
+                IsAutoUpdate = true
+            };
+
+            var exitCode = selfUpdateCommand.Run(new[] {"self-update"});
+            if (exitCode != 0)
+            {
+                log.LogError("Auto update cement failed. 'self-update' exited with code '{Code}'", exitCode);
+                consoleWriter.WriteWarning("Auto update failed. Check previous warnings for details");
+            }
+        }
+        catch (Exception exception)
+        {
+            log.LogError(exception, "Auto update failed, error: '{ErrorMessage}'", exception.Message);
+            consoleWriter.WriteWarning("Auto update failed. Check logs for details");
+        }
+    }
+}
--- Commands/SelfUpdateCommand.cs
+++ Commands/SelfUpdateCommand.cs
@@ -7,49 +7,10 @@ using System.Net;
 using System.Runtime.CompilerServices;
 using Common;
 using Common.Exceptions;
-using Common.Logging;
 using Common.Updaters;
 using Microsoft.Extensions.Logging;
 
 namespace Commands;
-
-public static class SelfUpdate
-{
-    public static void UpdateIfOld(FeatureFlags featureFlags)
-    {
-        var consoleWriter = ConsoleWriter.Shared;
-        var log = LogManager.GetLogger(nameof(SelfUpdate));
-
-        try
-        {
-            var isEnabledSelfUpdate = CementSettingsRepository.Get().IsEnabledSelfUpdate;
-            if (isEnabledSelfUpdate.HasValue && !isEnabledSelfUpdate.Value)
-                return;
-            var lastUpdate = Helper.GetLastUpdateTime();
-            var now = DateTime.Now;
-            var diff = now - lastUpdate;
-            if (diff <= TimeSpan.FromHours(5))
-                return;
-
-            var selfUpdateCommand = new SelfUpdateCommand(consoleWriter, featureFlags)
-            {
-                IsAutoUpdate = true
-            };
-
-            var exitCode = selfUpdateCommand.Run(new[] {"self-update"});
-            if (exitCode != 0)
-            {
-                log.LogError("Auto update cement failed. 'self-update' exited with code '{Code}'", exitCode);
-                consoleWriter.WriteWarning("Auto update failed. Check previous warnings for details");
-            }
-        }
-        catch (Exception exception)
-        {
-            log.LogError(exception, "Auto update failed, error: '{ErrorMessage}'", exception.Message);
-            consoleWriter.WriteWarning("Auto update failed. Check logs for details");
-        }
-    }
-}
 
 public sealed class SelfUpdateCommand : Command
 {
