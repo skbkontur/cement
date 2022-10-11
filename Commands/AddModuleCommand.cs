0217512437f55ad58f2654dec54db0191436f78c
Denis Tarasov <dstarasov@skbkontur.ru>
2022-10-11 18:18:38 +0500

    refactor: разбил ModuleCommand на Add..Command и Change..Command

---
+++ Commands/AddModuleCommand.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using Common;
+using Common.Exceptions;
+using JetBrains.Annotations;
+
+namespace Commands;
+
+[PublicAPI]
+public sealed class AddModuleCommand : Command<AddModuleCommandOptions>
+{
+    private static readonly CommandSettings Settings = new()
+    {
+        LogFileName = "module-add",
+        Location = CommandLocation.Any
+    };
+    private readonly ConsoleWriter consoleWriter;
+    private readonly IPackageUpdater packageUpdater;
+    private readonly ModuleHelper moduleHelper;
+
+    public AddModuleCommand(ConsoleWriter consoleWriter, FeatureFlags featureFlags, IPackageUpdater packageUpdater,
+                            ModuleHelper moduleHelper)
+        : base(consoleWriter, Settings, featureFlags)
+    {
+        this.consoleWriter = consoleWriter;
+        this.packageUpdater = packageUpdater;
+        this.moduleHelper = moduleHelper;
+    }
+
+    public override string Name => "add";
+    public override string HelpMessage => @"";
+
+    protected override int Execute(AddModuleCommandOptions options)
+    {
+        packageUpdater.UpdatePackages();
+        var packages = Helper.GetPackages();
+
+        var packageName = options.PackageName;
+        if (packages.Count > 1 && packageName == null)
+            throw new CementException($"Specify --package={string.Join("|", packages.Select(p => p.Name))}");
+
+        var package = packageName == null
+            ? packages.FirstOrDefault(p => p.Type == "git")
+            : packages.FirstOrDefault(p => p.Name == packageName);
+
+        if (package == null)
+            throw new CementException("Unable to find " + packageName + " in package list");
+
+        if (package.Type == "git")
+            return moduleHelper.AddModule(package, options.ModuleName, options.PushUrl, options.FetchUrl);
+
+        consoleWriter.WriteError("You should add local modules file manually");
+        return -1;
+    }
+
+    protected override AddModuleCommandOptions ParseArgs(string[] args)
+    {
+        var parsedArgs = ArgumentParser.ParseModuleCommand(args);
+        var moduleName = (string)parsedArgs["module"];
+        var pushUrl = (string)parsedArgs["pushurl"];
+        var fetchUrl = (string)parsedArgs["fetchurl"];
+        var packageName = (string)parsedArgs["package"];
+
+        return new AddModuleCommandOptions(moduleName, pushUrl, fetchUrl, packageName);
+    }
+}
--- Commands/ModuleCommand.cs
+++ Commands/ModuleCommand.cs
@@ -1,7 +1,6 @@
 using System;
-using System.Linq;
+using System.Collections.Generic;
 using Common;
-using Common.Exceptions;
 using JetBrains.Annotations;
 
 namespace Commands;
@@ -10,17 +9,18 @@ namespace Commands;
 public sealed class ModuleCommand : ICommand
 {
     private readonly ConsoleWriter consoleWriter;
-    private readonly ModuleHelper moduleHelper;
+    private readonly ICommandActivator commandActivator;
+    private readonly Dictionary<string, Type> commands;
 
-    private string command;
-    private string moduleName;
-    private string pushUrl, fetchUrl;
-    private Package package;
-
-    public ModuleCommand(ConsoleWriter consoleWriter, ModuleHelper moduleHelper)
+    public ModuleCommand(ConsoleWriter consoleWriter, ICommandActivator commandActivator)
     {
         this.consoleWriter = consoleWriter;
-        this.moduleHelper = moduleHelper;
+        this.commandActivator = commandActivator;
+        commands = new Dictionary<string, Type>
+        {
+            {"add", typeof(AddModuleCommand)},
+            {"change", typeof(ChangeModuleCommand)}
+        };
     }
 
     public string HelpMessage => @"
@@ -37,83 +37,15 @@ public sealed class ModuleCommand : ICommand
 
     public int Run(string[] args)
     {
-        try
+        if (args.Length < 2 || !commands.ContainsKey(args[1]))
         {
-            ParseArgs(args);
-            return Execute();
-        }
-        catch (CementException e)
-        {
-            consoleWriter.WriteError(e.Message);
+            consoleWriter.WriteError("Bad arguments");
             return -1;
         }
-        catch (Exception e)
-        {
-            consoleWriter.WriteError(e.Message);
-            consoleWriter.WriteError(e.StackTrace);
-            return -1;
-        }
-    }
 
-    private static Package GetPackage(string packageName)
-    {
-        PackageUpdater.Shared.UpdatePackages();
-        var packages = Helper.GetPackages();
+        var commandType = commands[args[1]];
+        var command = (ICommand)commandActivator.Create(commandType);
 
-        if (packages.Count > 1 && packageName == null)
-            throw new CementException($"Specify --package={string.Join("|", packages.Select(p => p.Name))}");
-
-        var package = packageName == null
-            ? packages.FirstOrDefault(p => p.Type == "git")
-            : packages.FirstOrDefault(p => p.Name == packageName);
-        if (package == null)
-            throw new CementException("Unable to find " + packageName + " in package list");
-        return package;
-    }
-
-    private int Execute()
-    {
-        if (package.Type != "git")
-        {
-            consoleWriter.WriteError("You should add/change local modules file manually");
-            {
-                return -1;
-            }
-        }
-
-        switch (command)
-        {
-            case "add":
-            {
-                return AddModule();
-            }
-            case "change":
-            {
-                return ChangeModule();
-            }
-        }
-
-        return -1;
-    }
-
-    private int AddModule()
-    {
-        return moduleHelper.AddModule(package, moduleName, pushUrl, fetchUrl);
-    }
-
-    private int ChangeModule()
-    {
-        return moduleHelper.ChangeModule(package, moduleName, pushUrl, fetchUrl);
-    }
-
-    private void ParseArgs(string[] args)
-    {
-        var parsedArgs = ArgumentParser.ParseModuleCommand(args);
-        command = (string)parsedArgs["command"];
-        moduleName = (string)parsedArgs["module"];
-        pushUrl = (string)parsedArgs["pushurl"];
-        fetchUrl = (string)parsedArgs["fetchurl"];
-        var packageName = (string)parsedArgs["package"];
-        package = GetPackage(packageName);
+        return command.Run(args);
     }
 }
