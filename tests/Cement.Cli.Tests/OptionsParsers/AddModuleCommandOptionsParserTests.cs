a2e28c2549e03aed139c27580f267f9e38296add
Denis Tarasov <dstarasov@skbkontur.ru>
2023-03-18 12:08:05 +0500

    test: добавил тесткейсы для AddModuleCommandOptionsParser

--- tests/Cement.Cli.Tests/OptionsParsers/AddModuleCommandOptionsParserTests.cs
+++ tests/Cement.Cli.Tests/OptionsParsers/AddModuleCommandOptionsParserTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Cement.Cli.Commands;
 using Cement.Cli.Commands.OptionsParsers;
 using Cement.Cli.Common.Exceptions;
@@ -21,8 +22,28 @@ public sealed class AddModuleCommandOptionsParserTests
     {
         get
         {
-            //todo(dstarasov): нужны тесткейсы
-            yield break;
+            const string module = "module";
+            const string fetchUrl = "fetch-url";
+
+            var args1 = new[] {"module", "add", module, fetchUrl};
+            var expected1 = new AddModuleCommandOptions(module, null, fetchUrl, null);
+            yield return new TestCaseData(args1, expected1) {TestName = "<module> <fetch-url>"};
+
+            const string pushUrl = "push-url";
+
+            var args2 = new[] {"module", "add", module, fetchUrl, "-p", pushUrl};
+            var expected2 = new AddModuleCommandOptions(module, pushUrl, fetchUrl, null);
+            yield return new TestCaseData(args2, expected2) {TestName = "<module> <fetch-url> -p <push-url>"};
+
+            const string package = "package";
+
+            var args3 = new[] {"module", "add", module, fetchUrl, "--package", package};
+            var expected3 = new AddModuleCommandOptions(module, null, fetchUrl, package);
+            yield return new TestCaseData(args3, expected3) {TestName = "<module> <fetch-url> --package <package>"};
+
+            var args4 = new[] {"module", "add", module, fetchUrl, "-p", pushUrl, "--package", package};
+            var expected4 = new AddModuleCommandOptions(module, pushUrl, fetchUrl, package);
+            yield return new TestCaseData(args4, expected4) {TestName = "<module> <fetch-url> -p <push-url> --package <package>"};
         }
     }
 
@@ -30,8 +51,18 @@ public sealed class AddModuleCommandOptionsParserTests
     {
         get
         {
-            //todo(dstarasov): нужны тесткейсы
-            yield break;
+            var args1 = (object)Array.Empty<string>();
+            yield return new TestCaseData(args1) {TestName = "no_arguments"};
+
+            const string module = "module";
+
+            var args2 = (object)new[] {"module", "add", module};
+            yield return new TestCaseData(args2) {TestName = "no_required_arguments"};
+
+            const string fetchUrl = "fetch-url";
+
+            var args3 = (object)new[] {"add", "add", module, fetchUrl, "--extra_argument1", "--extra_arguments2"};
+            yield return new TestCaseData(args3) {TestName = "extra_arguments"};
         }
     }
 
